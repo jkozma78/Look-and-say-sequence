bf7779374f58b46cc30a7b1f0e61f29b8f594e0f
Kozma János <67119975+jkozma78@users.noreply.github.com>
2022-01-18 19:26:52 +0100

    Add files via upload

--- sayit.py
+++ sayit.py
@@ -1,36 +1,39 @@
 class LookAndSaySequence:
-    def __init__(self, input, seq):
-        self.input = input
-        self.count = 0
-        self.conway = ""
-        self.currentnumber = input[0]
-        self.seq = seq
-        self.sequence()
+    """This class is calculating Look-and-say sequence. The first argument is the first number of sequence.
+    This method can return the elements(th) of the second argument or its' length."""
+
+    def __init__(self, start_number, elements):
+        """Init the arguments"""
+        self.start_number = str(start_number)  # the first number of sequence
+        self.number_count = 0  # counting the number
+        self.conway = ""  # created conway number
+        self.current_number = self.start_number[0]  # the actual number to count
+        self.elements = elements  # elements(th) number of sequence
+        self.calculation()  # call the function, what is able to calculate the elements(th) number of conway sequence
 
     def __str__(self):
-        return self.input
+        return self.start_number
 
     def __len__(self):
-        return len(self.input)
+        return len(self.start_number)
 
-    def sequence(self):
-        print(self.input)
-
-        for n in range(1, self.seq):
-            for i in range(len(self.input)):
-                if self.input[i] == self.currentnumber:
-                    self.count += 1
+    def calculation(self):
+        """Calculate the conway sequence"""
+        for n in range(1, self.elements):
+            # calculate the self.elements(th) number
+            for i in range(len(self.start_number)):
+                if self.start_number[i] == self.current_number:
+                    self.number_count += 1
                 else:
-                    self.conway = self.conway + str(self.count) + self.currentnumber
-                    self.currentnumber = self.input[i]
-                    self.count = 1
-                if i == len(self.input) - 1:
-                    self.conway = self.conway + str(self.count) + self.currentnumber
-
-            print(self.conway)
-            self.input = self.conway
-            self.count = 0
+                    self.conway = self.conway + str(self.number_count) + self.current_number
+                    self.current_number = self.start_number[i]
+                    self.number_count = 1
+                if i == len(self.start_number) - 1:
+                    self.conway = self.conway + str(self.number_count) + self.current_number
+            # reset these variables
+            self.start_number = self.conway
+            self.number_count = 0
             self.conway = ""
-            self.currentnumber = self.input[0]
+            self.current_number = self.start_number[0]
 
-        return self.input
+        return self.start_number
