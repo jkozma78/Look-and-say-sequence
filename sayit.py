505b206417ce8897a4a1e240f50ca0c9974e4bf9
Kozma János <67119975+jkozma78@users.noreply.github.com>
2022-01-31 11:45:37 +0100

    Add files via upload

--- sayit.py
+++ sayit.py
@@ -1,6 +1,6 @@
 class LookAndSaySequence:
     """This class is calculating Look-and-say sequence. The first argument is the first number of sequence.
-    This method can return the elements(th) of the second argument or its' length."""
+    This method can return the elements of the second argument or its length."""
 
     def __init__(self, start_number, elements):
         """Init the arguments"""
@@ -8,11 +8,11 @@ class LookAndSaySequence:
         self.number_count = 0  # counting the number
         self.conway = ""  # created conway number
         self.current_number = self.start_number[0]  # the actual number to count
-        self.elements = elements  # elements(th) number of sequence
-        self.calculation()  # call the function, what is able to calculate the elements(th) number of conway sequence
+        self.elements = elements  # elements number of sequence
+        self.calculation()  # call the function, what is able to calculate the elements number of conway sequence
 
     def __str__(self):
-        return self.start_number
+        return f' {self.elements}th number: {self.start_number}'
 
     def __len__(self):
         return len(self.start_number)
@@ -20,7 +20,7 @@ class LookAndSaySequence:
     def calculation(self):
         """Calculate the conway sequence"""
         for n in range(1, self.elements):
-            # calculate the self.elements(th) number
+            # calculate the self.elements number
             for i in range(len(self.start_number)):
                 if self.start_number[i] == self.current_number:
                     self.number_count += 1
@@ -31,6 +31,7 @@ class LookAndSaySequence:
                 if i == len(self.start_number) - 1:
                     self.conway = self.conway + str(self.number_count) + self.current_number
             # reset these variables
+            #print(self.start_number)
             self.start_number = self.conway
             self.number_count = 0
             self.conway = ""
